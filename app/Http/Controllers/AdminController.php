e64eb2e684b9527b2e8a145e6797b482d5d813d9
daudassidiqi <assidiqidaud@gmail.com>
2024-06-14 01:08:21 +0700

    Multi User Login System

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            # code...
+            $usertype = Auth()->user()->usertype;
+
+            if ($usertype == 'user') {
+                # code...
+                return view('dashboard');
+            } elseif ($usertype == 'admin') {
+                # code...
+                return view('admin.index');
+            } else {
+
+                return redirect()->back();
+            }
+        }
+    }
+}
